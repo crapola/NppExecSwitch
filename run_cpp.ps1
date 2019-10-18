4c58b4e3bf842ee1b17f148c4a52715d3667b389
crapola <crapola@users.noreply.github.com>
2019-10-18 16:45:21 +0200

    Output in cpp folder

--- run_cpp.ps1
+++ run_cpp.ps1
@@ -1,6 +1,9 @@
 $env:path+="F:\Applications\Mingw64\bin;"
-$out=$env:temp+"\a.exe"
+# Some antiviruses don't like exe's in temp folder.
+# $out=$env:temp+"\a.exe"
+$out=".\a.exe"
 write-host "Compiling to $out"
-g++ $args[0] -o $out -Wall -std=c++17 -pedantic
+g++ -v
+g++ $args[0] -o $out -Wall -std=c++2a -pedantic -g
 write-host "Running $out"
 & $out
