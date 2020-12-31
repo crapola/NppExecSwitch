f76c7c7f45e6131bf5eaf6387613b76973bc8dfe
Crapola <crapola@users.noreply.github.com>
2021-01-01 00:49:05 +0100

    Don't run after errors

--- run_rs.ps1
+++ run_rs.ps1
@@ -4,4 +4,7 @@ $proj_path=($args -split "src")[0]
 $proj_name=split-path $proj_path -leaf
 cd $proj_path
 & $cargo_path build
-& "$($proj_path)target\debug\$($proj_name).exe"
+if ( -not $LASTEXITCODE)
+{
+	& "$($proj_path)target\debug\$($proj_name).exe"
+}
