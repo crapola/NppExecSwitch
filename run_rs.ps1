ecc8f80d57c821afec44bb34ada8fe5f37e873d2
Crapola <crapola@users.noreply.github.com>
2021-01-01 00:32:14 +0100

    Rust

--- run_rs.ps1
+++ run_rs.ps1
@@ -1,1 +1,7 @@
-D:\Users\Utilisateur\.cargo\bin\rustc.exe $args[0]
+$cargo_path="F:\Rust\bin\cargo.exe"
+# Assumes project made with Cargo and we are in src/.
+$proj_path=($args -split "src")[0]
+$proj_name=split-path $proj_path -leaf
+cd $proj_path
+& $cargo_path build
+& "$($proj_path)target\debug\$($proj_name).exe"
